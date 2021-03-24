617b9918ada3eb83fa7060cf7faa8857a539346c
John Føreland <john.foreland@soprasteria.com>
2021-03-24 09:38:20 +0100

    Revert "removed everything"
    This reverts commit 2d38d8f9b816a43e83a4ba59db54e36fd0109f31.

--- src/main/kotlin/no/nav/crm/kafka/activity/EMP.kt
+++ src/main/kotlin/no/nav/crm/kafka/activity/EMP.kt
@@ -1,5 +1,13 @@
 package no.nav.crm.kafka.activity
 
+import mu.KotlinLogging
+// import com.fasterxml.jackson.databind.ObjectMapper
+// import com.fasterxml.jackson.module.kotlin.readValue
+import java.util.function.Consumer
+// import org.eclipse.jetty.util.ajax.JSON
+
+private val log = KotlinLogging.logger { }
+
 object EMP {
 
     @Throws(Throwable::class)
@@ -13,5 +21,22 @@ object EMP {
 
         println("-------------------")
         println("username: $username")
+    }
+
+    fun processData(): Consumer<Map<String, Any>> {
+        return Consumer<Map<String, Any>> { event ->
+            /*
+            val eventKey = JSON.toString(event.get("event"))
+            val sobject = JSON.toString(event.get("sobject"))
+
+            val map = ObjectMapper().readValue<MutableMap<Any, String>>(eventKey)
+            val replayId = map.get("replayId")
+*/
+            // val producer = KafkaProducer<String, String>(kafkaProducerConfig)
+            // producer.use { p ->
+            // p.send(ProducerRecord(topic, replayId, sobject))
+            // p.flush()
+            // }
+        }
     }
 }
