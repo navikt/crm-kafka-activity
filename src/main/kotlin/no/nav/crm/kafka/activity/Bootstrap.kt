a7dba91184c4368d3e2fc38344b58c7594ec7a48
Björn Hägglund <bjornhagglund@Bjorns-MacBook-Pro.local>
2021-03-26 15:22:02 +0100

    Let the main thread loop with 10 min suspension intervals after EMP init

--- src/main/kotlin/no/nav/crm/kafka/activity/Bootstrap.kt
+++ src/main/kotlin/no/nav/crm/kafka/activity/Bootstrap.kt
@@ -1,6 +1,12 @@
 package no.nav.crm.kafka.activity
 
 import com.salesforce.emp.connector.EmpConnector
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import mu.KotlinLogging
+import no.nav.crm.kafka.activity.nais.PrestopHook
+import no.nav.crm.kafka.activity.nais.ShutdownHook
 import no.nav.crm.kafka.activity.nais.enableNAISAPI
 
 // Environment dependencies injected in pod by nais kafka solution
@@ -9,10 +15,13 @@ val EMP_USERNAME = System.getenv("EMP_USERNAME")
 val EMP_PASSWORD = System.getenv("EMP_PASSWORD")
 val EMP_TOPIC = System.getenv("EMP_TOPIC")
 
+private val log = KotlinLogging.logger { }
+
 object Bootstrap {
 
     fun start() {
         enableNAISAPI {
+            log.info { "Before EMP processEvents" }
             EMP.processEvents(
                 EMP_URL,
                 EMP_USERNAME,
@@ -20,6 +29,39 @@ object Bootstrap {
                 "/topic/$EMP_TOPIC",
                 EmpConnector.REPLAY_FROM_EARLIEST
             )
+            log.info { "After EMP processEvents" }
+            loop()
+        }
+    }
+
+    private tailrec fun loop() {
+        val stop = ShutdownHook.isActive() || PrestopHook.isActive()
+        when {
+            stop -> Unit
+            !stop -> {
+                conditionalWait(600000) // Suspended wait (should not hog resources)
+                loop()
+            }
         }
     }
 }
+
+fun conditionalWait(ms: Long) =
+        runBlocking {
+            log.info { "Will wait $ms ms" }
+            val cr = launch {
+                runCatching { delay(ms) }
+                        .onSuccess { log.info { "waiting completed" } }
+                        .onFailure { log.info { "waiting interrupted" } }
+            }
+            tailrec suspend fun loop(): Unit = when {
+                cr.isCompleted -> Unit
+                ShutdownHook.isActive() || PrestopHook.isActive() -> cr.cancel()
+                else -> {
+                    delay(250L)
+                    loop()
+                }
+            }
+            loop()
+            cr.join()
+        }
